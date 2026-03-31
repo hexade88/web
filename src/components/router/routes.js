d8549ecf015e2dbcd38e772d326e33bb5fb60765
antonov <antonov@polymetal.ru>
2026-03-31 12:52:22 +0300

    dgthrthrth

--- src/components/router/routes.js
+++ src/components/router/routes.js
@@ -1,9 +1,9 @@
 
 const routes = [
-  { path:'/bx24', component: () => import('@/components/startPage.vue') },
+  { path:'/', component: () => import('@/components/startPage.vue') },
    { path:'/about', component: () => import('@/components/about.vue') },
-  /*{ path:'/users', component: () => import('@/components/users.vue') },
-  { path:'/deal', component: () => import('@/components/deal.vue') },
+  { path:'/bx24', component: () => import('@/components/startPage.vue') },
+  /*{ path:'/deal', component: () => import('@/components/deal.vue') },
   { path:'/dealload', component: () => import('@/components/dealload.vue') },
   { path:'/idload', component: () => import('@/components/idload.vue') },
   { path:'/company', component: () => import('@/components/company.vue') },
